29c2fa2bfe622cb542b9469416d6ad85f7541539
Henrique Merlin <henmerlin@gmail.com>
2018-03-15 09:37:43 -0300

    + FactoryNetworkInventoryService

---
+++ stealer/src/main/java/br/net/gvt/efika/stealer/model/service/FactoryNetworkInventoryService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.net.gvt.efika.stealer.model.service;
+
+/**
+ *
+ * @author G0042204
+ */
+public class FactoryNetworkInventoryService {
+
+    public static NetworkInventoryService newNetworkInventoryService() {
+        return new NetworkInventoryServiceImpl();
+    }
+
+}
