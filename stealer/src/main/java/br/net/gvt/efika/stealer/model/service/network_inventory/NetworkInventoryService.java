cb5661f82b7ca0c5f272b9fab62b88151425bada
Henrique Merlin <henmerlin@gmail.com>
2018-03-19 15:20:32 -0300

    fix package

--- stealer/src/main/java/br/net/gvt/efika/stealer/model/service/network_inventory/NetworkInventoryService.java
+++ stealer/src/main/java/br/net/gvt/efika/stealer/model/service/network_inventory/NetworkInventoryService.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.net.gvt.efika.stealer.model.service;
+package br.net.gvt.efika.stealer.model.service.network_inventory;
 
 import br.net.gvt.efika.customer.model.dto.CustomerRequest;
-import br.net.gvt.efika.efika_customer.model.customer.InventarioRede;
+import br.net.gvt.efika.efika_customer.model.customer.EfikaCustomer;
 
 /**
  *
@@ -15,6 +15,6 @@ import br.net.gvt.efika.efika_customer.model.customer.InventarioRede;
 public interface NetworkInventoryService {
 
 
-    public InventarioRede consultar(CustomerRequest cust) throws Exception;
+    public EfikaCustomer consultar(CustomerRequest cust) throws Exception;
 
 }
--- stealer/src/main/java/br/net/gvt/efika/stealer/model/service/network_inventory/NetworkInventoryServiceImpl.java
+++ stealer/src/main/java/br/net/gvt/efika/stealer/model/service/network_inventory/NetworkInventoryServiceImpl.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.net.gvt.efika.stealer.model.service;
+package br.net.gvt.efika.stealer.model.service.network_inventory;
 
 import br.net.gvt.efika.customer.model.dto.CustomerRequest;
-import br.net.gvt.efika.efika_customer.model.customer.InventarioRede;
+import br.net.gvt.efika.efika_customer.model.customer.EfikaCustomer;
 import br.net.gvt.efika.util.dao.http.Urls;
 import br.net.gvt.efika.util.dao.http.factory.FactoryHttpDAOAbstract;
 
 public class NetworkInventoryServiceImpl implements NetworkInventoryService {
 
     @Override
-    public InventarioRede consultar(CustomerRequest req) throws Exception {
-        return (InventarioRede) new FactoryHttpDAOAbstract<>(InventarioRede.class).createWithoutProxy().post(Urls.NETWORK_INVENTORY.getUrl(), req);
+    public EfikaCustomer consultar(CustomerRequest req) throws Exception {
+        return (EfikaCustomer) new FactoryHttpDAOAbstract<>(EfikaCustomer.class).createWithoutProxy().post(Urls.NETWORK_INVENTORY.getUrl(), req);
     }
 
 }
