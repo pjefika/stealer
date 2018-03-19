52defe9e10a7fa7737794b31c67ec8abb3252791
Henrique Merlin <henmerlin@gmail.com>
2018-03-19 10:54:01 -0300

    update

--- stealer/src/main/java/br/net/gvt/efika/stealer/model/service/service_inventory/FactoryServiceInventoryService.java
+++ stealer/src/main/java/br/net/gvt/efika/stealer/model/service/service_inventory/FactoryServiceInventoryService.java
@@ -5,15 +5,13 @@
  */
 package br.net.gvt.efika.stealer.model.service.service_inventory;
 
-import br.net.gvt.efika.efika_customer.model.customer.enums.OrigemInventarioServico;
-
 /**
  *
  * @author G0042204
  */
 public class FactoryServiceInventoryService {
 
-    public static ServiceInventoryService create(OrigemInventarioServico origem) throws Exception {
+    public static ServiceInventoryService create() throws Exception {
         return new GenericServiceInventoryServiceImpl();
         // throw new Exception("Provedor de Inventário de Serviços indisponível para: " + origem.name());
     }
