1e3355bf79772a1b90897a5eefe61ee7cf0b648e
Henrique Merlin <henmerlin@gmail.com>
2018-03-19 12:51:19 -0300

    change to GenericRequest

--- stealer/src/main/java/br/net/gvt/efika/stealer/model/service/NetworkInventoryServiceImpl.java
+++ stealer/src/main/java/br/net/gvt/efika/stealer/model/service/NetworkInventoryServiceImpl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.net.gvt.efika.stealer.model.service.network_inventory;
+package br.net.gvt.efika.stealer.model.service;
 
 import br.net.gvt.efika.customer.model.dto.CustomerRequest;
 import br.net.gvt.efika.efika_customer.model.customer.InventarioRede;
